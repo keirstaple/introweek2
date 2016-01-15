27cbc9e98c73c028406f21175eccfcb228111bea
Keir Staple <keirstaple@comcast.net>
2016-01-15 13:19:01 -0800

    add results section and begin the javascript for the form

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,15 @@ $(document).ready(function() {
     $('.questions').show()
   });
 
-  
+  $('.form').submit(function(event) {
+    var active = $('select.active').val()
+    var setting = $('select.setting').val()
+    var monkey = $('input[name=monkey]: checked').val()
+    var carbs = $('select.carbs').val()
+    var celebrity = $('input[name=celebrity]: checked').val()
+
+  event.preventDefualt();
+  });
+
+
 });
